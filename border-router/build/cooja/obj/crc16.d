971fa7a5e737dda1f81f002ec541cd05d0389380
lorepas <pascolorenzo@gmail.com>
2021-04-22 11:04:21 -0400

    adding border router

---
+++ border-router/build/cooja/obj/crc16.d
@@ -0,0 +1,4 @@
+build/cooja/obj/crc16.o: ../../../contiki-ng/os/lib/crc16.c \
+ ../../../contiki-ng/os/services/rpl-border-router/module-macros.h
+../../../contiki-ng/os/lib/crc16.c :
+ ../../../contiki-ng/os/services/rpl-border-router/module-macros.h :
