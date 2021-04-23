d6deb8e69cf7c422ff928b6d80a403508ed75260
lorepas <pascolorenzo@gmail.com>
2021-04-23 05:29:58 -0400

    adding coap server

---
+++ server/iot.unipi.it/src/main/java/iot/unipi/it/MyServer.java
@@ -0,0 +1,12 @@
+package iot.unipi.it;
+
+import org.eclipse.californium.core.CoapServer;
+
+public class MyServer extends CoapServer {
+
+	public static void main(String[] args) {
+		System.out.print("Running...");
+
+	}
+
+}
