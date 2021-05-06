afba8d9306b2f1a7fe78a106f3ab751e0e030ff9
lorepas <pascolorenzo@gmail.com>
2021-05-06 12:49:43 -0400

    fixed last things

--- coap-server/src/main/java/App.java
+++ coap-server/src/main/java/App.java
@@ -10,7 +10,6 @@ import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
 
 public class App {
-	//gestire meglio il changesprinkler e vedere come mai non funziona get sulle risorse
 	public static Map<String,Sprinkler> sprinkler = new HashMap<String,Sprinkler>();
 	public static Map<String,HumiditySensor> hum_sensor = new HashMap<String,HumiditySensor>();
 	public static Map<String,TemperatureSensor> temp_sensor = new HashMap<String,TemperatureSensor>();
@@ -26,7 +25,7 @@ public class App {
 			BufferedReader num_res = new BufferedReader(new InputStreamReader(System.in));
 			res_number = Integer.parseInt(num_res.readLine());
 			if(res_number<=0)
-				System.out.print("Please, try with a number greater than 0: ");
+				System.out.println("Please, try with a number greater than 0: ");
 		}
 		System.out.println("-------- WAITING RESOURCE REGISTRATION --------\n");
 		startServer();
@@ -51,29 +50,41 @@ public class App {
 				case(1):
 					System.out.println("-------- THIS IS A LIST OF REGISTERED SPRINKLER: --------\n");
 					showRegisteredSprinkler();
-					System.out.println("Please, select a Sprinkler ID:\n");
+					System.out.println("Please, select a Sprinkler ID:");
 					System.out.print(">>>>");
 					BufferedReader command_type = new BufferedReader(new InputStreamReader(System.in));
-					int nodeId = Integer.parseInt(command_type.readLine());
+					String s = command_type.readLine();
+					if(s.equals("q")) {
+						break;
+					}
+					int nodeId = Integer.parseInt(s);
 					changeSprinklerStatus(nodeId);
 					break;
 				case(2):
 					System.out.println("Which resources are you looking for?\n");
-					System.out.println("0 - Sprinklers\n");
-					System.out.println("1 - Humidity sensors\n");
-					System.out.println("2 - Temperature sensors\n");
+					System.out.println("0 - Sprinklers");
+					System.out.println("1 - Humidity sensors");
+					System.out.println("2 - Temperature sensors");
 					System.out.print(">>>>");
 					BufferedReader command_res = new BufferedReader(new InputStreamReader(System.in));
-					int res = Integer.parseInt(command_res.readLine());
+					String s1 = command_res.readLine();
+					if(s1.equals("q")) {
+						break;
+					}
+					int res = Integer.parseInt(s1);
 					showResourcesState(res);
 					break;
 				case(3):
-					System.out.println("Which sensors do you want to change threshold?\n");
-					System.out.println("0 - Humidity sensors\n");
-					System.out.println("1 - Temperature sensors\n");
+					System.out.println("Which sensors do you want to change threshold?");
+					System.out.println("0 - Humidity sensors");
+					System.out.println("1 - Temperature sensors");
 					System.out.print(">>>>");
 					BufferedReader command_sens = new BufferedReader(new InputStreamReader(System.in));
-					int sens = Integer.parseInt(command_sens.readLine());
+					String s2 = command_sens.readLine();
+					if(s2.equals("q")) {
+						break;
+					}
+					int sens = Integer.parseInt(s2);
 					changeResourcesStatus(sens);
 					break;
 				case(5):
@@ -87,15 +98,15 @@ public class App {
 	}
 	
 	public static void commandLine() {
-		System.out.println("\n***********************\n");
-		System.out.println("****** MAIN MENU ******\n");
-		System.out.println("***********************\n");
-		System.out.println("Please insert a command:...\n");
-		System.out.println("0 - Show all registered resources\n");
-		System.out.println("1 - Change sprinkler status\n");
-		System.out.println("2 - Show resources status\n");
-		System.out.println("3 - Change sensors thresholds\n");
-		System.out.println("5 - Stop application\n");
+		System.out.println("\n***********************");
+		System.out.println("****** MAIN MENU ******");
+		System.out.println("***********************");
+		System.out.println("Please insert a command:...");
+		System.out.println("0 - Show all registered resources");
+		System.out.println("1 - Change sprinkler status");
+		System.out.println("2 - Show resources status");
+		System.out.println("3 - Change sensors thresholds");
+		System.out.println("5 - Stop application");
 		System.out.println("To quit from a menu, please digit: q");
 		System.out.print(">>>>");
 	}
@@ -187,7 +198,6 @@ public class App {
 	public static void changeSprinklerStatus(int id) {
 		if(id >= sprinkler.entrySet().size() || id < 0) {
 			System.out.println("-------- NODE IS NOT PRESENT! RETRY... --------\n");
-			commandLine();
 			return;
 		}
 		String[] keys = sprinkler.keySet().toArray(new String[0]);
@@ -213,28 +223,26 @@ public class App {
 			showRegisteredTemperatureSensor();
 		}else {
 			System.out.println("-------- WRONG SELECTION! RETRY... --------\n");
-			commandLine();
 			return;
 		}
-		System.out.println("Please, select a resource ID:\n");
+		System.out.println("Please, select a resource ID:");
 		System.out.print(">>>>");
 		BufferedReader res_id = new BufferedReader(new InputStreamReader(System.in));
 		int id = Integer.parseInt(res_id.readLine());
 		if(res==0) {
 			if(id >= hum_sensor.entrySet().size() || id < 0) {
 				System.out.println("-------- NODE IS NOT PRESENT! RETRY... --------\n");
-				commandLine();
 				return;
 			}
 			String[] keys = hum_sensor.keySet().toArray(new String[0]);
-			System.out.println("Please, digit a new humidity threshold (from 0 to 100):\n");
+			System.out.println("Please, digit a new humidity threshold (from 0 to 100):");
 			System.out.print(">>>>");
 			int thr_hum = -1;
 			while(thr_hum<0||thr_hum>100) {
 				BufferedReader res_thr = new BufferedReader(new InputStreamReader(System.in));
 				thr_hum = Integer.parseInt(res_thr.readLine());
 				if(res_number<0||thr_hum>100)
-					System.out.print("Please, try with another number: ");
+					System.out.println("Please, try with another number >>>> ");
 			}
 			CoapClient client = new CoapClient(hum_sensor.get(keys[id]).getResURI());
 			CoapResponse response = client.post("thr_hum="+thr_hum, MediaTypeRegistry.TEXT_PLAIN);
@@ -248,18 +256,17 @@ public class App {
 		}else if(res==1) {
 			if(id >= temp_sensor.entrySet().size() || id < 0) {
 				System.out.println("-------- NODE IS NOT PRESENT! RETRY... --------\n");
-				commandLine();
 				return;
 			}
 			String[] keys = temp_sensor.keySet().toArray(new String[0]);
-			System.out.println("Please, digit a new temperature threshold (from 0 to 32):\n");
+			System.out.println("Please, digit a new temperature threshold (from 0 to 32):");
 			System.out.print(">>>>");
 			int thr_tmp = -1;
 			while(thr_tmp<0||thr_tmp>32) {
 				BufferedReader res_thr = new BufferedReader(new InputStreamReader(System.in));
 				thr_tmp = Integer.parseInt(res_thr.readLine());
 				if(res_number<0||thr_tmp>32)
-					System.out.print("Please, try with another number: ");
+					System.out.println("Please, try with another number >>>> ");
 			}
 			CoapClient client = new CoapClient(temp_sensor.get(keys[id]).getResURI());
 			CoapResponse response = client.post("thr_tmp="+thr_tmp, MediaTypeRegistry.TEXT_PLAIN);
